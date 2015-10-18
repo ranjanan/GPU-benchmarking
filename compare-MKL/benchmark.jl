85dc8e75f11ab98778a592c7def79edc8b036a81
Ranjan Anantharaman <ranjan@juliacomputing.com>
2015-10-18 08:36:30 +0000

    Comparing spmv MKL and no MKL

---
+++ compare-MKL/benchmark.jl
@@ -0,0 +1,19 @@
+function benchmark(k)
+
+	println("----------- k = $k ---------") 
+
+	a = sprand(10^7, 10^7, k*1e-7)
+	
+	b = rand(10^7)
+
+	println("Time taken for multiplication:")
+	@time ch = a * b
+
+end
+
+function run_benchmark()
+	println("--------- No MKL -------------")
+	for k = 1:9
+		benchmark(k)
+	end
+end
