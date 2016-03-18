7b1daab3f0ba4386127f07f99c79c423b3dfb2e9
Ranjan Anantharaman <ranjan@juliacomputing.com>
2016-03-18 10:57:03 +0000

    Clustering works for small matrices

---
+++ graph/cluster.jl
@@ -0,0 +1,16 @@
+function cluster(G::SparseMatrixCSC)
+    n = size(a, 1)
+    G = map(Int, map(Bool,a) | map(Bool, speye(n)))
+    val = maximum(a,2)
+    leader = Array(Int, n)
+    for i = 1:n
+        leader[i] = findin(a[i,:], val[i])[1]
+    end
+    S = G * sparse(collect(1:n), leader, 1)
+    val = maximum(a,2)
+    leader = Array(Int, n)
+    for i = 1:n
+        leader[i] = findin(a[i,:], val[i])[1]
+    end
+    leader
+end
