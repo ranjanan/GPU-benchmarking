8a534333ff5033ac989176c535b73011249369e1
Ranjan Anantharaman <ranjan@juliacomputing.com>
2016-04-09 19:06:00 +0000

    Add symv benchmark

---
+++ symv/benchmark.jl
@@ -0,0 +1,18 @@
+using CUBLAS, CUDArt
+function benchmark()
+	for k in [0.5, 0.75, 1, 1.25]
+		info("--- k = $k ---")
+		dim = Int(k * 10^4)
+		a = rand(dim, dim)
+		a = a * a'
+		b = rand(dim)
+		@time ad = CudaArray(a)
+		@time bd = CudaArray(b)
+		t1 = @elapsed a * b
+		t2 = @elapsed CUBLAS.symv('U', ad, bd)
+		t3 = @elapsed CUBLAS.symv('U', a, b)
+		println("Time taken for CPU version = $t1")
+		println("Time taken for GPU version = $t2")
+		println("Time taken for GPU with tranfer version = $t3")
+	end
+end
