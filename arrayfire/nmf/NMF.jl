5ba6e9324567677a86adccb718fb52d8eb505803
Ranjan Anantharaman <benditlikeranjan@gmail.com>
2016-06-09 09:13:44 +0000

    Modify benchmarks

--- arrayfire/nmf/NMF.jl
+++ arrayfire/nmf/NMF.jl
@@ -16,17 +16,18 @@ function nmf_gpu{T<:Real}(A::AFArray{T}, k::Integer, n::Int=10)
         H = H .* (W'*A) ./ ((W'*W)*H)
         W = W .* (A*H') ./ (W*(H*H'))
     end
+    sync()
     return W, H
 end
 
 info("Warmup")
-a = rand(Float32, 10,10)
+a = rand(Float32,10,10)
 ad = AFArray(a)
 nmf(a,2)
 nmf_gpu(ad,2)
 for i = 1:10
     dim = i*1000
-    a = rand(Float32, dim,dim)
+    a = rand(Float32,dim,dim)
     ad = AFArray(a)
     info("Start Timing Dim = $dim !")
     t1 = @elapsed nmf(a,2)
@@ -34,6 +35,7 @@ for i = 1:10
     println("tcpu = $t1")
     println("tgpu = $t2")
     println("Speedup = $(t1/t2)")
+    gc()
 end
 #=a = rand(1000,1000)
 ad = AFArray(a)
